4d63367c5918fa35987079d809b35cde9da5db3e
AkshayKamate851 <kamateakshay@outlook.com>
2024-07-03 01:40:36 +0530

    Create main.tf
    that contain the private key also to authentecate with the instance

---
+++ main.tf
@@ -0,0 +1,182 @@
+variable "private_key" {
+  default = <<EOF
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn
+NhAAAAAwEAAQAAAYEAurOtZB7yO1oqFnW1xAOD/D32PCSTLRhkRIkAPVli177DhgN/lZtb
+zhY8F7/n6uT8/5V14rEdtdRs7NHmnuyTR/inpk9F+HjqJhk2Prd8MYZWKPlz5wZEynlRPN
+jSK8OyxLxlk0YHDQxsnRurVog3z/iYTgaWp0l5Tz2B4e7+NWzqT3j1xIGMdn6eowe9jEzn
+BgS/+KFykcLXfInJV1PkjqB8qyYAiMJwZ/PzaIvtDyIA3F1IFmkRSQU0AI/d85tbH+myzk
+f0X7V/BGtBeENHrQHswayVKLBOOAJC8tJEwTWUJwY8paHaOQRF+80TZH7Ih2o80ScqFHFq
+XTyXCQmuXPp5irJlwvNzXyzyld99muSJEhSZUPgxIHGsKkwkIKngea/kGZNRVPmz6oRDZA
+Ax1FTKQJABPGUkO1uNpwTI00Vq/fhju2QiFmYvYOtgEdQGNZC4MSEfuzMdW6ewhuOewwFy
+4VDpX4fHnFaW2E8RS2s+f5GN+GNCU9p1v8cvbE2xAAAFiOUb8O3lG/DtAAAAB3NzaC1yc2
+EAAAGBALqzrWQe8jtaKhZ1tcQDg/w99jwkky0YZESJAD1ZYte+w4YDf5WbW84WPBe/5+rk
+/P+VdeKxHbXUbOzR5p7sk0f4p6ZPRfh46iYZNj63fDGGVij5c+cGRMp5UTzY0ivDssS8ZZ
+NGBw0MbJ0bq1aIN8/4mE4GlqdJeU89geHu/jVs6k949cSBjHZ+nqMHvYxM5wYEv/ihcpHC
+13yJyVdT5I6gfKsmAIjCcGfz82iL7Q8iANxdSBZpEUkFNACP3fObWx/pss5H9F+1fwRrQX
+hDR60B7MGslSiwTjgCQvLSRME1lCcGPKWh2jkERfvNE2R+yIdqPNEnKhRxal08lwkJrlz6
+eYqyZcLzc18s8pXffZrkiRIUmVD4MSBxrCpMJCCp4Hmv5BmTUVT5s+qEQ2QAMdRUykCQAT
+xlJDtbjacEyNNFav34Y7tkIhZmL2DrYBHUBjWQuDEhH7szHVunsIbjnsMBcuFQ6V+Hx5xW
+lthPEUtrPn+RjfhjQlPadb/HL2xNsQAAAAMBAAEAAAGAAWbP6myBDQkEP97qWmpcV0xvyM
+vNfw0r04fjxJIjN9NqzFrCXnEoI0fvc44xQ4YskA6jL+x8eJz7pFt/C7pi8KthNtJpiBAA
+UgDmc1oVSJnlk7EruZRGd+6pOXe3PDTf5rIzYQOpkeOB/Tvz8VZcdOc83XkXMHJQwtOHYh
+bVOblzmN0jmzINx+Xzcu3f8mmxYk4u0sMDhSydSYYPSOSMVrXFudMzNdOf3Gfsb+B9CxwK
+PZ1FqUVop8+uMELkO6vy4+X0VgQtbue9N67ht9N/1EhpDtQrZRzzNrNvRAs5v1cHNILbPj
+nkAqHftgS4dAa+PzH19vtrULaKIg8Go+XUFqvsdViG3t7Y8B4ECvR6UsJaJh6FAgBuo/2o
+ca1AadxdtsiQW/PtTEMgrPqRXMUmB7jzeE5gwUnBsat2bMoCHfz/I/ObYMTbHuW+ebVL+E
+MYKGdebnHtzho6qWSLyg8X4OXAPaVEM3yy4uGIcxQ9SOXeP8qDYHheqnptLhE2RWFBAAAA
+wQC5HloOWE4BR7vm/uoFmSWKOdrp8d2MTAaHqdU3JZM8qgqGEQf6BsYpGv1BwIXie4Blq7
+7Q8RZaWU2G7fCymu4ftYI5TAdYOJlcY3OhK8A4Ahz36YXkzN//peqBVvim8SgbYDKkx4Vp
+J5DQ47s8jqycPJliF1YiovJBtPKCiwQGfpNZBsdjgy40fKwioopHOZM698Oz1o5uq3myor
+xQUQcsrpUnQNvrGbTr7l3jhRNqqZHRRUkLSzdL5HTQaj5ZcjYAAADBAN7pu7M++b8ivE/K
+ukfqRfvAdWz9bT8Ni/Bv08o+A/e0vODRZUC8wMqCZ6XmjPsIe4LiveZOpjP+lNn/0fm6Yl
+jAGzXVYP9hWD2Pam+Btd6FMI1mteCW2CbMnvLjq2eRNs91wtUIP+PU7ie4Y88lI/ABb8zx
+bmRkX5nYx7/dxbM/KjpgtEPXlIUswRhIxOwt5rUymKyU9vC9r7EjNy7jlCIFye5JZqSN1I
+McR1x6ZZPWvwUlfvVOsxaxu0qkdEY7ZQAAAMEA1mn9XV6ZSIxz6rBzDFUF9+Uw9mMSbqjV
+MWGKfJ26P7vf1tMl65btc8VVBpYmSJRWamesNyI/IAD2gI2TqynXNBdwZxUZ4/pFS8kn56
+gKvAVgmST6D+QipCfygXPfKymIzoFXHNmBu9lbjyhQUPKSpFXa1o9zzEokq1DFxaBL+PDa
+SXKSIhgOvHtRbjeWLGE4vPi+1Yfd2QKPKeOZSgMyLanlnv45fiaA2H1CrWxqmuHzKu+dEc
+gAOoJm8tm1ozJdAAAAEXRzcDE5QFRFSkFTLVBBVElMAQ==
+-----END OPENSSH PRIVATE KEY-----
+EOF
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main_vpc"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main_gw"
+  }
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "main_subnet_a"
+  }
+}
+
+resource "aws_route_table" "rtable" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "main_rtable"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet_a.id
+  route_table_id = aws_route_table.rtable.id
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins_sg"
+  }
+}
+
+resource "aws_instance" "jenkins" {
+  ami                         = "ami-04b70fa74e45c3917"
+  instance_type               = "t2.micro"
+  key_name                    = "Universal_key"
+  subnet_id                   = aws_subnet.subnet_a.id
+  security_groups             = [aws_security_group.jenkins_sg.id]
+  iam_instance_profile        = "ec2-role"
+  associate_public_ip_address = true  
+  tags = {
+    Name = "Jenkins"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install -y docker.io",
+      "curl -sfL https://get.k3s.io | sh -",
+      "sleep 30",
+      "sudo k3s kubectl get node"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = var.private_key
+      host        = self.public_ip
+    }
+  }
+   depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_instance" "k8s" {
+  ami                         = "ami-04b70fa74e45c3917"
+  instance_type               = "t2.micro"
+  key_name                    = "Universal_key"
+  subnet_id                   = aws_subnet.subnet_a.id
+  security_groups             = [aws_security_group.jenkins_sg.id]
+  iam_instance_profile        = "kubernetes-role"
+  associate_public_ip_address = true  
+
+  tags = {
+    Name = "K8s"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install -y docker.io",
+      "curl -sfL https://get.k3s.io | sh -",
+      "sleep 30",
+      "sudo k3s kubectl get node"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = var.private_key
+      host        = self.public_ip
+    }
+  }
+
+ depends_on = [aws_internet_gateway.gw]
+}
